e31329a3ccd863d9562b24aeb89d9b0fad62e0ae
Renato Galvez <53193744+Renforce79@users.noreply.github.com>
2023-04-16 12:34:00 -0500

    updating comments

--- load_data_DAG.py
+++ load_data_DAG.py
@@ -4,28 +4,27 @@ from datetime import datetime
 import os
 dag_folder = os.path.dirname(os.path.abspath(__file__))
 
-
-# Define el DAG
+# Define DAG
 dag = DAG(
     'ejecutar_miproyecto',
     start_date=datetime(2023, 4, 12),
     schedule_interval=None
 )
 
-# Crea el operador BashOperator
+# Creating crear_tablas  BashOperator
 crear_tablas = BashOperator(
     task_id='crear_tablas',
     bash_command=f'python {dag_folder}/create_tables.py',
     dag=dag
 )
 
-# Crea el operador BashOperator
+# Creating ejecutar_script  BashOperator
 ejecutar_script = BashOperator(
     task_id='ejecutar_script',
     bash_command=f'python {dag_folder}/load_data.py',
     dag=dag
 )
 
-# Define el orden en el que se deben ejecutar las tareas
+# Define task order
 crear_tablas >> ejecutar_script
 
