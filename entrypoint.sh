418fe25f824517472005650ec91b039ebe95763c
Renato Galvez <53193744+Renforce79@users.noreply.github.com>
2023-04-16 01:43:30 -0500

    changing arflow comands

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,13 +11,33 @@ su postgres -c "psql -c \"ALTER USER postgres PASSWORD 'postgres';\""
 # Initialize the Airflow database
 airflow db init
 
-# Start the Airflow web server and scheduler
-airflow webserver -D &
-airflow scheduler -D &
+# Start the Airflow web server
+airflow webserver -p 3000 -D
 
-# Wait for the Airflow web server and scheduler to start
-echo "Waiting for Airflow web server and scheduler to start..."
-sleep 30
+# Wait for the Airflow web server to start
+echo "Waiting for Airflow web server to start..."
+while true; do
+    _RUNNING=$(ps aux | grep airflow-webserver | grep -v grep)
+    if [ -z "${_RUNNING}" ]; then
+        sleep 1
+    else
+        break
+    fi
+done
+
+# Start the Airflow scheduler
+airflow scheduler -D
+
+# Wait for the Airflow scheduler to start
+echo "Waiting for Airflow scheduler to start..."
+while true; do
+    _RUNNING=$(ps aux | grep airflow-scheduler | grep -v grep)
+    if [ -z "${_RUNNING}" ]; then
+        sleep 1
+    else
+        break
+    fi
+done
 
 # Trigger the Airflow DAG with the specified file path
-airflow dags trigger ejecutar_miproyecto -r ejecutar_miproyecto --conf '{"file_path":"/code/BING_MultiDays.csv"}'
+airflow dags trigger ejecutar_miproyecto
